323a62edcc2d1aa7b0222f70927f46d705217a58
Benjamin Meyer <Benjamin.Meyer@EXXETA.com>
2019-10-03 12:28:30 +0200

    Added licence header and for now using the thirdparty dependencies out of the lib folder

--- bundles/org.openhab.binding.philipstv/src/main/java/org/openhab/binding/philipstv/internal/handler/PhilipsTvHandlerFactory.java
+++ bundles/org.openhab.binding.philipstv/src/main/java/org/openhab/binding/philipstv/internal/handler/PhilipsTvHandlerFactory.java
@@ -1,10 +1,14 @@
 /**
- * Copyright (c) 2010-2019 by the respective copyright holders.
- * <p>
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
  */
 package org.openhab.binding.philipstv.internal.handler;
 
@@ -17,8 +21,6 @@ import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
 import org.openhab.binding.philipstv.internal.PhilipsTvDynamicStateDescriptionProvider;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.Set;
@@ -33,8 +35,6 @@ import static org.openhab.binding.philipstv.internal.PhilipsTvBindingConstants.T
  */
 @Component(service = ThingHandlerFactory.class, configurationPid = "binding.philipstv")
 public class PhilipsTvHandlerFactory extends BaseThingHandlerFactory {
-
-    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_PHILIPS_TV);
 
